e6fbb768bb13468b58327e6d2520c301f5155ee4
root <root@DESKTOP-2VVGB42.localdomain>
2021-09-28 19:12:04 -0600

    Add token file

---
+++ token/token.go
@@ -0,0 +1,31 @@
+type TokenType string
+
+type Token struct {
+	Type TokenType
+	Literal string
+}
+
+const {
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	// Identifiers and literals
+	IDENT = "IDENT"
+	INT = "INT"
+
+	// Operators
+	ASSIGN = "="
+	PLUS = "+"
+
+	//delimiters
+	COMMA = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	KEYWORDS = "FUNCTION"
+	LET = "LET"
+}
